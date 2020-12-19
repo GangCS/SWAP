bc45c644214aa16c9d5457d6dc0d666d633b4385
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2020-12-19 20:15:57 +0200

    Added some comments

--- Assets/Scripts/ButtonPushed.cs
+++ Assets/Scripts/ButtonPushed.cs
@@ -4,34 +4,30 @@ using UnityEngine;
 
 public class ButtonPushed : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField]
-    GameObject Button;
-    [SerializeField]
-    GameObject Door;
+    [SerializeField] GameObject Button;
+    [SerializeField] GameObject Door;
     Quaternion lookRotation;
     void Start()
     {
         
     }
     //transform.localScale = new Vector3(1, 1, 0.2f);
-    // transform.localScale = new Vector3(1, 1, 1);
-    private void OnTriggerEnter(Collider other)
-    {
-        Button.transform.localScale = new Vector3(1, 1, 0.2f);
-        lookRotation = Quaternion.Euler(new Vector3(0, -90, 0));
+    //transform.localScale = new Vector3(1, 1, 1);
 
-        //
+    private void OnTriggerEnter(Collider other)//Button is pushed
+    {
+        Button.transform.localScale = new Vector3(1, 1, 0.2f);//Pushing the Button down
+        lookRotation = Quaternion.Euler(new Vector3(0, -90, 0));
        // Door.transform.rotation = lookRotation;
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)//Button is no longer pushed
     {
-        Button.transform.localScale = new Vector3(1, 1, 1);
+        Button.transform.localScale = new Vector3(1, 1, 1);//Pulling Button back up
         lookRotation = Quaternion.Euler(new Vector3(0, 0, 0));
     }
-    // Update is called once per frame
+
     void Update()
     {
-        Door.transform.rotation = Quaternion.Slerp(Door.transform.rotation, lookRotation, Time.deltaTime * 5f);
+        Door.transform.rotation = Quaternion.Slerp(Door.transform.rotation, lookRotation, Time.deltaTime * 5f);// Opens the door
     }
 }
