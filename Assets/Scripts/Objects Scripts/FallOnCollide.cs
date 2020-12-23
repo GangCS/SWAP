3f37c93c375db40444c21faec7bfc71fb61dd2bd
Vadim Kachevski <vadimkache@gmail.com>
2020-12-23 16:48:10 +0200

    maybe fix box collider

--- Assets/Scripts/Objects Scripts/FallOnCollide.cs
+++ Assets/Scripts/Objects Scripts/FallOnCollide.cs
@@ -17,13 +17,17 @@ public class FallOnCollide : MonoBehaviour
             pickedup = true;
             pickUpTime = Time.time;
         }
+        if(!GetComponent<Rigidbody>().isKinematic && pickedup)
+        {
+            pickedup = false;
+        }
     }
     // "When the Box Collide with something its falling"
     private void OnCollisionEnter(Collision collision)
     {
         if (GetComponent<Rigidbody>().isKinematic && pickedup)
         {
-            if (Time.time - pickUpTime > 1f)
+            if (Time.time - pickUpTime > 0.5f)
             {
                 transform.SetParent(null);
                 GetComponent<Rigidbody>().isKinematic = false;
