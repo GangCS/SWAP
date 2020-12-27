6e8363adb48ce1646fa48579ae51b4da1a10a0a9
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-12-27 10:50:13 +0200

    moving platform and design changes

---
+++ Assets/Scripts/Objects Scripts/HoldingPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingPlayer : MonoBehaviour
+{
+/*    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.parent = gameObject.transform;
+        Debug.Log("Player stand on platform");
+    }*/
+    void OnTriggerStay(Collider col)
+    {
+        col.transform.parent = gameObject.transform;
+        Debug.Log("Player stand on platform");
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        col.transform.parent = null;
+        Debug.Log("Player exit from platform");
+    }
+}
